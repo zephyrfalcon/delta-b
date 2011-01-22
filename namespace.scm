f524e482269e786df58b8d84a11a2f5ed3105754
zephyrfalcon <zephyrfalcon@gmail.com>
2011-01-22 18:06:08 -0500

    added tests for namespace module

--- namespace.scm
+++ namespace.scm
@@ -6,9 +6,9 @@
 
 ;; a namespace is represented by a pair (parent . slots).
 (define (make-namespace parent)
-  (cons parent (make-hash-table string=?)))
+  (cons parent (make-hash-table 'string=?)))
 (define (namespace-parent ns) (car ns))
-(define (namespace-slots) (cdr ns))
+(define (namespace-slots ns) (cdr ns))
 
 (define (namespace-set! ns name value)
   (hash-table-put! (namespace-slots ns) name value))
@@ -31,16 +31,33 @@
 (define (namespace-get-local ns name)
   (hash-table-get (namespace-slots ns) name #f))
 
+;; look for the name in the namespace and its parents, then rebind
+;; that name in the namespace where it was found. returns #t if found
+;; and replaced, #f otherwise.
 (define (namespace-update! ns name value)
-  ...)
+  (receive (old-value in-ns)
+      (namespace-get ns name)
+    (if old-value
+        (begin
+          (namespace-set! in-ns name value)
+          #t)
+        #f)))
 
+;; delete the name from the namespace (locally, i.e. parent namespaces
+;; are unaffected even if the name exists there). returns #t if found
+;; and deleted, #f otherwise.
 (define (namespace-delete! ns name)
-  ...)
+  (hash-table-delete! (namespace-slots ns) name))
 
+;; return a list of all the names defined in the namespace. order is undefined.
 (define (namespace-names-local ns)
-  ...)
+  (hash-table-keys (namespace-slots ns)))
 
 (define (namespace-names-all ns)
-  ...)
+  (let* ((names (namespace-names-local ns))
+         (parent (namespace-parent ns)))
+    (if parent
+        (append names (namespace-names-all parent))
+        names)))
 
 
--- test-namespace.scm
+++ test-namespace.scm
@@ -4,3 +4,44 @@
 (use gauche.test)
 (use namespace)
 
+(test-start "namespace")
+
+(let ((ns (make-namespace #f)))
+  (test* "" #f (namespace-parent ns))
+  (namespace-set! ns "foo" 1)
+  (namespace-set! ns "bar" 2)
+  (test* "" 1 (namespace-get ns "foo"))
+  (test* "" #f (namespace-get ns "bletch"))
+  (test* "names in namespace (local)"
+         '("bar" "foo") (sort (namespace-names-local ns) string<?))
+  (test* "names in namespace (all)"
+         '("bar" "foo") (sort (namespace-names-all ns) string<?))
+
+  (let ((ns2 (make-namespace ns)))
+    (namespace-set! ns2 "foo" 3)
+    (namespace-set! ns2 "baz" 4)
+    (test* "" 3 (namespace-get ns2 "foo"))
+    (test* "" 2 (namespace-get ns2 "bar"))
+    (test* "" 4 (namespace-get ns2 "baz"))
+
+    ;; "foo" can be found in ns2
+    (receive (value origin)
+        (namespace-get ns2 "foo")
+      (test* "" #t (eq? ns2 origin)))
+    ;; "bar" is found in ns
+    (receive (value origin)
+        (namespace-get ns2 "bar")
+      (test* "" #t (eq? ns origin)))
+
+    (namespace-update! ns2 "foo" 5)
+    (namespace-update! ns2 "bar" 6)
+    (test* "" 6 (namespace-get ns "bar"))
+    (test* "" 5 (namespace-get ns2 "foo"))
+    (test* "" 1 (namespace-get ns "foo"))
+    )
+
+  
+)
+
+
+(test-end)
