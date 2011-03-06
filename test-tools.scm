0c7a51102f74d782c4d1b2fff7bbd13d8db326fb
zephyrfalcon <zephyrfalcon@gmail.com>
2011-03-06 16:32:49 -0500

    added string-slice

--- test-tools.scm
+++ test-tools.scm
@@ -9,4 +9,13 @@
 (test-section "unique")
 (test* "" '(1 2 3 6) (sort (unique '(1 2 3 1 2 3 6 1)) <))
 
+(test-section "string-slice")
+(test* "s[1:]"  "bcde" (string-slice "abcde" 1))
+(test* "s[1:0]" ""     (string-slice "abcde" 1 0))
+(test* "s[-1:]" "e"    (string-slice "abcde" -1))
+(test* "s[:-1]" "abcd" (string-slice "abcde" 0 -1))
+(test* "s[1:3]" "bc"   (string-slice "abcde" 1 3))
+(test* "s[9:]"  ""     (string-slice "abcde" 9))
+(test* "s[4:2]"  ""    (string-slice "abcde" 4 2))
+
 (test-end)
--- tools.scm
+++ tools.scm
@@ -22,4 +22,14 @@
 (define (assert x :optional (msg #f))
   (unless x (error (or msg "assertion failed"))))
 
-
+(define (string-slice s idx1 :optional (idx2 #f))
+  (let* ((sl (string-length s))
+         (idx2 (or idx2 sl)) ;; defaults to length of string
+         (corr-idx1 (if (negative? idx1) (+ sl idx1) idx1))
+         (corr-idx2 (if (negative? idx2) (+ sl idx2) idx2)))
+    #;(printf "(DEBUG) ~s, ~s~%" corr-idx1 corr-idx2)
+    (if (>= corr-idx1 corr-idx2)
+        ""
+        (substring s
+                   (min (max corr-idx1 0) sl)
+                   (min (max corr-idx2 0) sl)))))
