fd657b15bf6cdca584d6d0b606f88d82a4281ba1
zephyrfalcon <zephyrfalcon@gmail.com>
2011-03-15 22:35:34 -0400

    added Method proto

--- builtin/umethod.scm
+++ builtin/umethod.scm
@@ -11,9 +11,41 @@
   block   ;; instance of delta-block; comes with its own namespace
   )
 
+(define (new-umethod-object interp args block)
+  (let* ((umethod-proto (find-builtin-proto interp "Method"))
+         (umethod (make-delta-umethod args block))
+         (uobj (clone-object 'umethod :data umethod)))
+    uobj))
+
 ;;; --- methods ---
 
-(define *umethod-methods* '())
+;; Method new: [args...] block
+;; Create a new user-defined method with zero or more arguments.
+(define (m-umethod-new obj args ns interp)
+  ...)
+;; need: zero or more symbols
+;; then a block (already parsed & evaluated as a Delta Block object)
+;; everything else will be ignored
+;; pass this to new-umethod-object
+
+;; Method call: target [args...]
+;; A new namespace will be created to evaluate the method's code, based on
+;; the namespace associated with the method's block.
+;; Target will be bound to "~". Args will be bound to those defined on the
+;; method. For now: if not enough arguments are passed, then those not defined
+;; will default to Null; OTOH, superfluous arguments will be ignored.
+;; Other objects may be bound as well (e.g. maybe a shorthand to the current
+;; namespace?).
+(define (m-umethod-call obj args ns interp)
+  ...)
+
+(define (m-umethod-clone obj args ns interp)
+  ...)
+
+(define *umethod-methods*
+  (list (list "call" m-umethod-call)
+        (list "new" m-umethod-new)
+        ))
 
 ;;; --- Method proto ---
 
